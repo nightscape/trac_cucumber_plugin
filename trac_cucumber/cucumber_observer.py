633b92bb49d852b4ea6d112fcc2e3f062b6f7880
Martin Mauch <moe@x200.(none)>
2009-07-29 10:35:10 +0200

    Replaced os.system by subprocess.Popen

--- trac_cucumber/cucumber_observer.py
+++ trac_cucumber/cucumber_observer.py
@@ -1,19 +1,25 @@
 from interfaces import *
 from trac.config import Option
 import os
+import subprocess
 
 class CommandCucumberObserver(Component):
     implements(ICucumberObserver)
 
     story_added_callback = Option("cucumber", "story_added_callback")
     story_edited_callback = Option("cucumber", "story_edited_callback")
+    story_directory = PathOption("cucumber", "story_directory")
+    output_directory = PathOption("cucumber", "output_directory")
 
     def story_added(self, story_name, story):
         if self.story_added_callback:
             self.log.debug("Executing %s %s" % (self.story_added_callback, story_name))
-            os.system("%s %s" % (self.story_added_callback, story_name))
+            self.execute(self.story_added_callback,story_name,story)
 
     def story_edited(self, story_name, story):
         if self.story_edited_callback:
             self.log.debug("Executing %s %s" % (self.story_edited_callback, story_name))
-            os.system("%s %s" % (self.story_edited_callback, story_name))
+            self.execute(self.story_edited_callback,story_name,story)
+
+    def execute(self,command,story_name,story):
+        subprocess.Popen((command, story_name), executable=command,  stdout=open(os.path.join(output_directory,'output.txt'),"w"), stderr=open(os.path.join(output_directory,'error.txt'),"w"), shell=False, cwd=story_directory, env=None)
